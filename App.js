98b00d5481541d38072b6795255e7f86187094aa
piterpits123 <112430546+piterpits123@users.noreply.github.com>
2024-02-19 15:05:23 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import {exibirHorario} from './components/Horario';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="lnk"
+        >
+          Learning React
+        </a>
+        <button
+          onClick={exibirHorario}
+        >Ver Horario</button>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
